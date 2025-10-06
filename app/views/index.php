7c59e985bb138dc7f28b3699a1a21b0a5a32b4d4
Mae <marymae@gmail.com>
2025-10-06 15:28:56 +0800

    Latest commit

--- app/views/index.php
+++ app/views/index.php
@@ -52,6 +52,7 @@
     
     <center><h1 class="mb-4">Student Management</h1></center>
     <a href="<?=site_url('students/create');?>" class="btn btn-success btn-lg mt-3">Add Student</a><br><br>
+    <a href="<?=site_url('auth/dashboard');?>" class="btn btn-success btn-lg mt-3">Back To Dashboard</a>
 
     <table class="table table-striped table-bordered">
       <thead class="table-dark">
